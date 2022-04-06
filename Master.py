2c587d25b0cc8bcc7d6e2fa54df69f72febaa05f
kevinjcai <kevincai@berkeley.edu>
2022-04-06 14:48:24 -0700

    Master.py WIP 1.1.1

--- Master.py
+++ Master.py
@@ -8,7 +8,7 @@ def generateVideo_txt(path):
     folder = path.split('/')[-1]
     for i in range(len(dir_list)):
         dir_list[i] = path + dir_list[i]
-    txtName = name + '_' + folder + '_videos'
+    txtName = name + '/' + folder + '_videos.txt'
     with open(txtName, 'w') as filehandle:
         for listitem in dir_list:
             filehandle.write('%s\n' % listitem)
@@ -22,7 +22,7 @@ if __name__ == "__main__":
             txtName = generateVideo_txt(path=path)
             name = path.split('/')[-2]
             folder = path.split('/')[-1]
-            feature_folder = name + '/' + folder + '_Features'
+            feature_folder = name + '/' + folder + ''
             command = "python feat_extract.py --data-list {0} --model \
                 i3d_resnet50_v1_kinetics400 --save-dir {1}".format(txtName, feature_folder)
             os.system(command)
