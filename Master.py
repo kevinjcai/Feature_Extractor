0dcf88a9e1b9b7f68584825d3deba9cfa0efd7cb
kevinjcai <kevincai@berkeley.edu>
2022-04-06 15:29:19 -0700

    Master.py 1.2.2

--- Master.py
+++ Master.py
@@ -8,7 +8,13 @@ def generateVideo_txt(path):
     folder = path.split('/')[-1]
     for i in range(len(dir_list)):
         dir_list[i] = path + dir_list[i]
-    txtName = name + '/' + folder + '_videos.txt'
+    txtPath = '/home/kevincai/Feature_Extractor/' + name + '_holder'
+    isExist = os.path.exists(txtPath)
+    if not isExist:
+        # Create a new directory because it does not exist 
+        os.makedirs(path)
+        print("The new directory is created!")
+    txtName = name + '_holder/' + folder + '_videos.txt'
     with open(txtName, 'w') as filehandle:
         for listitem in dir_list:
             filehandle.write('%s\n' % listitem)
@@ -23,7 +29,7 @@ if __name__ == "__main__":
             txtName = generateVideo_txt(path=path)
             name = path.split('/')[-2]
             folder = path.split('/')[-1]
-            feature_folder = name + '/' + folder + ''
+            feature_folder = name + '_holder/' + folder + '_features'
             command = "python feat_extract.py --data-list {0} --model \
                 i3d_resnet50_v1_kinetics400 --save-dir {1}".format(txtName, feature_folder)
             os.system(command)
