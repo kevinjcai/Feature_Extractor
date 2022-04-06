fdce348652758dc03396cf4c9c3209e56511d03f
kevinjcai <kevincai@berkeley.edu>
2022-04-06 14:53:35 -0700

    Master.py WIP 1.1.6

--- Master.py
+++ Master.py
@@ -17,15 +17,15 @@ if __name__ == "__main__":
     print(sys.argv)
     if len(sys.argv) == 2:
         path = sys.argv[0]
-        if not os.path.exists(os.path.dirname(path)):
-            raise Exception("Directory does not exist")
-        else:
-            txtName = generateVideo_txt(path=path)
-            name = path.split('/')[-2]
-            folder = path.split('/')[-1]
-            feature_folder = name + '/' + folder + ''
-            command = "python feat_extract.py --data-list {0} --model \
-                i3d_resnet50_v1_kinetics400 --save-dir {1}".format(txtName, feature_folder)
-            os.system(command)
+        # if not os.path.exists(os.path.dirname(path)):
+        #     raise Exception("Directory does not exist")
+        # else:
+        txtName = generateVideo_txt(path=path)
+        name = path.split('/')[-2]
+        folder = path.split('/')[-1]
+        feature_folder = name + '/' + folder + ''
+        command = "python feat_extract.py --data-list {0} --model \
+            i3d_resnet50_v1_kinetics400 --save-dir {1}".format(txtName, feature_folder)
+        os.system(command)
     else:
         raise Exception("Need exactly 1 argument")
