f23efb73c52310cc0a8c10aa8c0a717a08025e8f
kevinjcai <kevincai@berkeley.edu>
2022-04-06 14:52:14 -0700

    Master.py WIP 1.1.5

--- Master.py
+++ Master.py
@@ -15,7 +15,7 @@ def generateVideo_txt(path):
     return txtName
 if __name__ == "__main__":
     print(sys.argv)
-    if len(sys.argv) == 1:
+    if len(sys.argv) == 2:
         path = sys.argv[0]
         if not os.path.exists(os.path.dirname(path)):
             raise Exception("Directory does not exist")
